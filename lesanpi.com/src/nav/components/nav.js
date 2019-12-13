03bd933a32371070051286597d992c08b83e7ef5
Luis Enrique Sanchez <lespinerua@gmail.com>
2019-12-13 17:27:52 -0400

    Actualizacion navbar 1

--- lesanpi.com/src/nav/components/nav.js
+++ lesanpi.com/src/nav/components/nav.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './nav.css'
 function Nav(props) {
 	return(
-		<div className="nav">
-			<div className="navComponent"><p>Navegador</p></div>
-			<div className="navComponent"><p>Navegador</p></div>
-			<div className="navComponent"><p>Navegador</p></div>
-			<div className="navComponent"><p>Navegador</p></div>
+		<div className="mainMenu">
+			<div className="innerMainMenu">
+				<ul>
+					<li><a>Home</a></li>
+					<li><a>Blog</a></li>
+					<li><a>Contact</a></li>
+					<li><a>Portfolio</a></li>
+				</ul>
+			</div>
 		</div>
 	)
 }
