22899be6deb87db4b0ba2e6807bd75b51e4d249f
Luis <lespinerua@gmail.com>
2023-09-03 14:08:44 -0400

    feat(failures): network failures

---
+++ failures/lib/src/network_failures/network_failure.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'network_failure.freezed.dart';
+part 'network_failure.g.dart';
+
+/// {@template network_failure}
+/// NetworkFailure description
+/// {@endtemplate}
+@freezed
+class NetworkFailure with _$NetworkFailure {
+  /// {@macro network_failure}
+  const factory NetworkFailure({
+    required String message,
+    required int code,
+    @Default([]) List<String> errors,
+  }) = _NetworkFailure;
+
+  /// Creates a NetworkFailure from Json map
+  factory NetworkFailure.fromJson(Map<String, dynamic> data) =>
+      _$NetworkFailureFromJson(data);
+}
