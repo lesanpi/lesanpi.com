476e5c10762d3f0a15d5b51e46a171418c138845
Luis <lespinerua@gmail.com>
2023-09-03 17:05:46 -0400

    feat(repository): abstract repositories

---
+++ repository/lib/src/post_repository.dart
@@ -0,0 +1,32 @@
+import 'package:either_dart/either.dart';
+import 'package:failures/failures.dart';
+import 'package:models/models.dart';
+import 'package:typedefs/typedefs.dart';
+
+/// {@template user_repository}
+/// Base class for all post repositories.
+/// {@endtemplate}
+abstract class PostRepository {
+  /// Get post with the given [id].
+  Future<Either<Failure, Post>> getPostById(PostId id);
+
+  /// Get all posts
+  Future<Either<Failure, List<Post>>> getAllPost();
+
+  /// Get all post with the tag [id]
+  Future<Either<Failure, List<Post>>> getAllPostByTagId(TagId id);
+
+  /// Get posts that belongs to the given [section]
+  Future<Either<Failure, List<Post>>> getPostsBySection(
+    PostSectionEnum section,
+  );
+
+  /// Create new post using the given [createPostDto]
+  Future<Either<Failure, Post>> createPost(CreatePostDto createPostDto);
+
+  /// Update post using the given [id] and [updatePostDto]
+  Future<Either<Failure, Post>> updatePost({
+    required PostId id,
+    required UpdatePostDto updatePostDto,
+  });
+}
